cf44799780e63c1cda450cfb8de6c7978e78a1c7
shoko-kurokawa <smiyamo2017@gmail.com>
2026-08-27 20:04:28 +0900

    feat: プロフィール機能実装　#8

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mypage/profile', function () {
-    return view('profile.edit');
-})->middleware('auth')->name('profile.edit');
+Route::middleware('auth')->group(function () {
+    Route::get('/mypage', [ProfileController::class, 'index'])
+        ->name('profile.index');
+
+    Route::get('/mypage/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/mypage/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+});
